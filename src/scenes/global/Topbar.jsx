c74bf4a3b547c6f6745184f752d0d6a04825eb9e
garciamarcoss <garciams1995@gmail.com>
2023-03-01 11:24:45 -0300

    first commit: folder architecture and theme settings

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,13 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+
+
+export const Topbar = () => {
+  return (
+    <div>Topasdasdasdasdbar</div>
+  );
+};
+
