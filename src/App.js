92d562491af3bf6b23d90cc23e0e073c3358529e
garciamarcoss <garciams1995@gmail.com>
2023-03-15 17:19:28 -0300

    form pages created

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Team } from "./scenes/team/Team";
 import {Invoices} from "./scenes/invoices/Invoices";
 import { Contacts } from "./scenes/contacts/Contacts";
 // import {Bar} from "./scenes/Bar";
-// import {Form} from "./scenes/Form";
+import {Form} from "./scenes/form/Form";
 // import {Line} from "./scenes/Line";
 // import {Pie} from "./scenes/Pie";
 // import {FAQ} from "./scenes/Faq";
@@ -31,7 +31,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
